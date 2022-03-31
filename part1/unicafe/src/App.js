38f175d05f02e21798349330d7b9ca7ca402258a
Jayson Cheng <jason.cheng@onelab.tw>
2022-03-31 14:23:37 +0800

    Exercise 1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -31,12 +31,19 @@ const App = () => {
 }
 
 const Statistics = (props) => {
+  const allCount = props.good + props.neutral + props.bad
+  const average = ((props.good * 1) - (props.bad * 1)) / allCount
+  const positive = (props.good / allCount) * 100
+
  return (
    <div>
      <h1>statistics</h1>
      <StatisticLine text="good" value ={props.good} />
      <StatisticLine text="neutral" value ={props.neutral} />
      <StatisticLine text="bad" value ={props.bad} />
+     <StatisticLine text="all" value ={allCount} />
+     <StatisticLine text="average" value={average} />
+     <StatisticLine text="positive" value={positive+" %"} />
    </div>
  ) 
 }
