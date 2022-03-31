2b2dfa01e600b738873ee56cc8e30cc356abe5bc
Jayson Cheng <jason.cheng@onelab.tw>
2022-03-31 18:09:11 +0800

    The Phonebook init

---
+++ part2/the-phonebook/src/App.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+
+const Note = ({ note }) => {
+  return <li>{note.name}</li>;
+};
+
+const App = (props) => {
+  const [notes, setNotes] = useState([{ name: "Arto Hellas", id: 1, important: true }]);
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      name: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5,
+      id: notes.length + 1,
+    }
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
+    console.log('notes:', notes)
+  }
+
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value)
+  }
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important === true)
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all' }
+        </button>
+      </div>
+      <ul>
+        {notesToShow.map((note) => (
+          <Note key={note.id} note={note} />
+        ))}
+      </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
+    </div>
+  );
+};
+
+export default App;
