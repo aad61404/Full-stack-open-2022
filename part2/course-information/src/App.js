5f58df4c15260389ac8580ba21e0cd80e6e3889e
Jayson Cheng <jason.cheng@onelab.tw>
2022-03-31 16:21:15 +0800

    Exercise 2.2

--- part2/course-information/src/App.js
+++ part2/course-information/src/App.js
@@ -14,11 +14,23 @@ const Part = ({ text, value }) => {
 
 const Content = ({ course }) => {
   console.log(course);
+  const totalStyle = {
+    fontWeight: "bold",
+  };
+
+  const totalExercises =
+    course.parts[0].exercises +
+    course.parts[1].exercises +
+    course.parts[2].exercises +
+    course.parts[3].exercises;
+
   return (
     <>
       <Part text={course.parts[0].name} value={course.parts[0].exercises} />
       <Part text={course.parts[1].name} value={course.parts[1].exercises} />
       <Part text={course.parts[2].name} value={course.parts[2].exercises} />
+      <Part text={course.parts[3].name} value={course.parts[3].exercises} />
+      <div style={totalStyle}>total of {totalExercises} exercises</div>
     </>
   );
 };
@@ -51,6 +63,11 @@ function App() {
         name: "State of a component",
         exercises: 14,
         id: 3,
+      },
+      {
+        name: "Redux",
+        exercises: 11,
+        id: 4,
       },
     ],
   };
