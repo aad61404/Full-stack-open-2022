fd9239c9359ec328a153f96abb2cad6280cf720d
Jayson Cheng <jason.cheng@onelab.tw>
2022-04-01 18:21:22 +0800

    Exercise 2.12* data countries

---
+++ part2/ReadMe.md
@@ -0,0 +1,6 @@
+npm install -g json-server
+npm install json-server --save-dev
+npx json-server --port 3001 --watch db.json
+
+<!-- API countries網站 -->
+https://restcountries.com/#api-endpoints-v3-all
---
+++ part2/countries/src/App.js
@@ -0,0 +1,82 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const Line = ({ name }) => {
+  return (
+    <tr>
+      <td>{name}</td>
+    </tr>
+  );
+};
+
+const Filter = ({ data, txt }) => {
+  // console.log('apiData:', apiData)
+  if (data) {
+    var result = data.filter(
+      (item) => item.name.toLowerCase().search(txt.toLowerCase()) !== -1
+    );
+
+    if (result.length === 1) {
+      const showData = result[0];
+      return (
+        <>
+          <h1>{showData.name}</h1>
+          <p>capital {showData.capital}</p>
+          <p>population {showData.population}</p>
+          <ul>
+            {showData.languages.map((language) => (
+              <li key={language.name}>{language.name}</li>
+            ))}
+          </ul>
+          <img src={showData.flag} alt={showData.name} width="100px" />
+        </>
+      );
+    }
+
+    if (result.length > 10) {
+      return <p>Too many matches, specify another filter</p>;
+    }
+
+    if (result.length > 1 && result.length < 10) {
+      return (
+        <>
+          <table>
+            <tbody>
+              {result.map((item) => (
+                <Line key={item.name} name={item.name} />
+              ))}
+            </tbody>
+          </table>
+        </>
+      );
+    }
+  }
+};
+
+function App() {
+  const [apiData, setApiData] = useState([]);
+  const [searchCountry, setSearchCountry] = useState("");
+
+  const handleSearchCountry = (event) => {
+    return setSearchCountry(event.target.value);
+  };
+
+  useEffect(() => {
+    console.log("effect");
+    axios.get(`https://restcountries.com/v2/all`).then((response) => {
+      console.log("type response.data :", typeof response.data);
+      setApiData(response.data);
+    });
+  }, []);
+
+  return (
+    <>
+      <div>
+        find countries <input type="text" onChange={handleSearchCountry} />
+      </div>
+      <Filter data={apiData} txt={searchCountry} />
+    </>
+  );
+}
+
+export default App;
