09668765f6ae58b858055f7f785c6beeca297a82
Jayson Cheng <jason.cheng@onelab.tw>
2022-04-01 12:18:53 +0800

    Exercise 2.8 phone nubmer

--- part2/the-phonebook/src/App.js
+++ part2/the-phonebook/src/App.js
@@ -1,11 +1,16 @@
 import React, { useState } from "react";
 
 const Note = ({ note }) => {
-  return <li>{note.name}</li>;
+  return (
+    <>
+      <li>{note.name} {note.phoneNumber}</li>
+    </>
+  );
 };
 
 const App = (props) => {
-  const [notes, setNotes] = useState([{ name: "Arto Hellas", id: 1,}]);
+  const [notes, setNotes] = useState([{ name: "Arto Hellas", id: 1, phoneNumber: '09123456789' }]);
+  const [phoneNumber, setPhoneNumber] = useState('')
   const [newNote, setNewNote] = useState('')
 
   const addNote = (event) => {
@@ -13,19 +18,25 @@ const App = (props) => {
     const noteObject = {
       name: newNote,
       id: notes.length + 1,
+      phoneNumber: phoneNumber
     }
 
     if (checkNotes(newNote)) {
       alert(newNote + 'is already added to phonebook')
     } else {
       setNotes(notes.concat(noteObject))
-      setNewNote('');
+      setNewNote('')
+      setPhoneNumber('')
     }
     console.log('notes:', notes)
   }
 
   const handleNoteChange = (event) => {
     setNewNote(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setPhoneNumber(event.target.value)
   }
 
   const checkNotes = (name) => {
@@ -39,7 +50,12 @@ const App = (props) => {
     <div>
       <h1>Phonebook</h1>
       <form onSubmit={addNote}>
-        <input value={newNote} onChange={handleNoteChange} />
+        <div>
+          name: <input value={newNote} onChange={handleNoteChange} />
+        </div>
+        <div>
+          number: <input value={phoneNumber} onChange={handleNumberChange} />
+        </div>
         <button type="submit">add</button>
       </form>
       <h1>Numbers</h1>
