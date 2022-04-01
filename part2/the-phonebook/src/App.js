bb89660688e59e58810378040bb0cae71c1582f2
Jayson Cheng <jason.cheng@onelab.tw>
2022-04-01 16:11:43 +0800

    Exercise 2.9

--- part2/the-phonebook/src/App.js
+++ part2/the-phonebook/src/App.js
@@ -9,26 +9,33 @@ const Note = ({ note }) => {
 };
 
 const App = (props) => {
-  const [notes, setNotes] = useState([{ name: "Arto Hellas", id: 1, phoneNumber: '09123456789' }]);
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', phoneNumber: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', phoneNumber: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', phoneNumber: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', phoneNumber: '39-23-6423122', id: 4 }
+  ])
+  // const [notes, setNotes] = useState([{ name: "Arto Hellas", id: 1, phoneNumber: '09123456789' }]);
   const [phoneNumber, setPhoneNumber] = useState('')
+  const [filterText, setFilterText] = useState('')
   const [newNote, setNewNote] = useState('')
 
   const addNote = (event) => {
     event.preventDefault()
     const noteObject = {
       name: newNote,
-      id: notes.length + 1,
+      id: persons.length + 1,
       phoneNumber: phoneNumber
     }
 
     if (checkNotes(newNote)) {
       alert(newNote + 'is already added to phonebook')
     } else {
-      setNotes(notes.concat(noteObject))
+      setPersons(persons.concat(noteObject))
       setNewNote('')
       setPhoneNumber('')
     }
-    console.log('notes:', notes)
+    console.log('persons:', persons)
   }
 
   const handleNoteChange = (event) => {
@@ -39,8 +46,16 @@ const App = (props) => {
     setPhoneNumber(event.target.value)
   }
 
+  const handleFilterTextChange = (event) => {
+    setFilterText(event.target.value)
+  }
+
+  const filterPerson = () => {
+    return persons.filter(e => e.name.includes(filterText) )
+  }
+
   const checkNotes = (name) => {
-    if (notes.filter(e => e.name === name).length > 0) {
+    if (persons.filter(e => e.name === name).length > 0) {
       return true
     }
     return false
@@ -49,6 +64,10 @@ const App = (props) => {
   return (
     <div>
       <h1>Phonebook</h1>
+      <div>
+        filter shown with <input value={filterText} onChange={handleFilterTextChange} />
+      </div>
+      <h1>add a new</h1>
       <form onSubmit={addNote}>
         <div>
           name: <input value={newNote} onChange={handleNoteChange} />
@@ -60,7 +79,7 @@ const App = (props) => {
       </form>
       <h1>Numbers</h1>
       <ul>
-        {notes.map((note) => (
+        {filterPerson().map((note) => (
           <Note key={note.id} note={note} />
         ))}
       </ul>
