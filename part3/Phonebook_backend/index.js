c16f72c031ed88aa923508a30fd892caff0fad3e
Jayson Cheng <jason.cheng@onelab.tw>
2022-04-19 19:26:18 +0800

    Exercise 3.9~3.11 heroku setup & Phonebook_f2E build & fix npm start

--- part3/Phonebook_backend/index.js
+++ part3/Phonebook_backend/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require('cors')
 const app = express();
+app.use(express.static('build'))
+app.use(cors())
+
 app.use(express.json())
 // app.use(morgan("tiny"));
 // sample
@@ -115,7 +119,8 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001;
+
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server runnning on port ${PORT}`)
 })
