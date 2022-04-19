d9a53e32e77e670dc1e8997e722658b87a35f376
Jayson Cheng <jason.cheng@onelab.tw>
2022-04-19 16:09:39 +0800

    Exercise 3.7 middleware morgan

--- part3/Phonebook_backend/index.js
+++ part3/Phonebook_backend/index.js
@@ -1,7 +1,28 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
-
 app.use(express.json())
+app.use(morgan("tiny"));
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
+app.use(morgan(function (tokens, req, res) {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms'
+  ].join(' ')
+}))
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('-----------------------')
+  next()
+}
+
+app.use(requestLogger)
 
 let persons = [
     { 
@@ -91,6 +112,11 @@ app.post("/api/persons", (req, res) => {
     res.json(person);
   });
 
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
 
 const PORT = 3001;
 app.listen(PORT, () => {
