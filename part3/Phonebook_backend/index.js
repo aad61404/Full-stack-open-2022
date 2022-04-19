aa2dae93e6cd04f8972d2900a6b2b51bb86d2659
Jayson Cheng <jason.cheng@onelab.tw>
2022-04-19 14:49:37 +0800

    Exercise 3.1 Phonebook backend step3 get id

--- part3/Phonebook_backend/index.js
+++ part3/Phonebook_backend/index.js
@@ -35,6 +35,19 @@ app.get("/api/persons", (req, res) => {
     res.json(persons)
 })
 
+app.get("/api/persons/:id", (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if (person) {
+        res.json(person)
+    } else {
+        res.status(404).json({
+            error: `can't find id`
+        })
+    }
+})
+
 app.get("/info", (req, res) => {
     var time = new Date()
     res.send(`<div><p>Pheonebook has info for 2 people</p><p>${time}</p></div>`)
