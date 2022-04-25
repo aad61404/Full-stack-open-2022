fc7d991968262e86a7ce4a03b80c8ffe989879d0
Jayson Cheng <jason.cheng@onelab.tw>
2022-04-25 19:20:26 +0800

    Exercise 4.18 webstoken login

--- part4/blogList/controllers/login.js
+++ part4/blogList/controllers/login.js
@@ -1,48 +1,50 @@
-const jwt = require('jsonwebtoken')
-const bcrypt = require('bcrypt')
-const loginRouter = require('express').Router()
-const User = require('../models/user')
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const loginRouter = require("express").Router();
+const User = require("../models/user");
 
-loginRouter.post('/', async (request, response) => {
-  const { username, password } = request.body
+loginRouter.post("/", async (request, response) => {
+  const { username, password } = request.body;
 
-  const user = await User.findOne({ username })
-  console.log('user:', user)
+  const user = await User.findOne({ username });
+  console.log("user:", user);
   // const saltRounds = 10
 
   const comparePassword = async (password, hash) => {
     try {
-        // Compare password
-        return await bcrypt.compare(password, hash);
+      // Compare password
+      return await bcrypt.compare(password, hash);
     } catch (error) {
-        console.log(error);
+      console.log(error);
     }
- 
+
     // Return false if error
     return false;
   };
 
-
-  const passwordCorrect = user === null ? false : await comparePassword(password, user.passwordHash);
+  const passwordCorrect =
+    user === null ? false : await comparePassword(password, user.passwordHash);
 
   if (!(user && passwordCorrect)) {
     return response.status(401).json({
-      error: 'invalid username or password'
-    })
+      error: "invalid username or password",
+    });
   }
-  
+
   const userForToken = {
     username: user.username,
     id: user._id,
-  }
-  console.log('comparePassword:', await comparePassword(password, user.passwordHash))
-    const token = jwt.sign(userForToken, process.env.SECRET)
-    console.log('token:', token)
+  };
+  console.log(
+    "comparePassword:",
+    await comparePassword(password, user.passwordHash)
+  );
+  const token = jwt.sign(userForToken, process.env.SECRET);
+  console.log("token:", token);
 
-    response
+  response
     .status(200)
-    .send({ token, username: user.username, name: user.name })
-  
-})
+    .send({ token, username: user.username, name: user.name });
+});
 
-module.exports = loginRouter
+module.exports = loginRouter;
