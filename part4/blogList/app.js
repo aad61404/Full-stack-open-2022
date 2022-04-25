1e8282c975e47a8396cafd530660ef49d8926edb
Jayson Cheng <jason.cheng@onelab.tw>
2022-04-25 18:12:12 +0800

    Exercise 4.15~4.16 bloglist get user

--- part4/blogList/app.js
+++ part4/blogList/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const blogsRouter = require("./controllers/Blog");
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const middleware = require("./utils/middleware");
 const logger = require("./utils/logger");
 const config = require("./utils/config");
@@ -24,7 +26,14 @@ app.use(cors());
 app.use(express.json());
 app.use(middleware.requestLogger);
 
+app.use('/api/login', loginRouter)
 app.use("/api/blogs", blogsRouter);
+app.use('/api/users', usersRouter)
+
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controllers/testing')
+  app.use('/api/testing', testingRouter)
+}
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
