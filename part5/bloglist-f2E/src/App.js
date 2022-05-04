0547b32571e4cf2ce1f728ab83be6b5cd24972e3
Jayson Cheng <jason.cheng@onelab.tw>
2022-05-04 17:09:10 +0800

    Exercise 5.6 Separate the form for creating a new blog into its own component

--- part5/bloglist-f2E/src/App.js
+++ part5/bloglist-f2E/src/App.js
@@ -1,51 +1,51 @@
 import React, { useState, useEffect } from "react";
 import Blog from "./components/Blog";
 import LoginForm from "./components/LoginForm";
-import BlogForm from './components/BlogForm'
+import BlogForm from "./components/BlogForm";
 import Notification from "./components/Notification";
 import blogService from "./services/blogs";
 import loginService from "./services/login";
-import Togglable from "./components/Togglable"
-import "./app.css"
+import Togglable from "./components/Togglable";
+import "./app.css";
 
 const App = () => {
   const [blogs, setBlogs] = useState([]);
   const [user, setUser] = useState(null);
-  const [errorMessage, setErrorMessage] = useState(null)
-  const blogRef = React.createRef()
+  const [errorMessage, setErrorMessage] = useState(null);
+  const blogRef = React.createRef();
 
   useEffect(() => {
     blogService.getAll().then((blogs) => setBlogs(blogs));
   }, []);
 
   useEffect(() => {
-    const loggedUser = window.localStorage.getItem('user')
+    const loggedUser = window.localStorage.getItem("user");
     if (loggedUser) {
-      setUser(JSON.parse(loggedUser))
+      setUser(JSON.parse(loggedUser));
     }
-  }, [])
+  }, []);
 
   const handleLogin = async (username, password) => {
     try {
       const user = await loginService.login({
         username,
         password,
-      })
-      blogService.setToken(user.token)
-      setUser(user)
-      window.localStorage.setItem('user', JSON.stringify(user))
+      });
+      blogService.setToken(user.token);
+      setUser(user);
+      window.localStorage.setItem("user", JSON.stringify(user));
       setErrorMessage({ text: `Weclome ${user.name}`, type: "success" });
     } catch (exception) {
       setErrorMessage({ text: `wrong username or password`, type: "error" });
     }
-  }
+  };
 
-  const handleLogout = e => {
-    e.preventDefault()
+  const handleLogout = (e) => {
+    e.preventDefault();
 
-    setUser(null)
-    window.localStorage.removeItem('user')
-  }
+    setUser(null);
+    window.localStorage.removeItem("user");
+  };
 
   const handleBlogForm = async (title, author, url) => {
     try {
@@ -53,16 +53,15 @@ const App = () => {
         title,
         author,
         url,
-      })
-      setBlogs(blogs.concat(blog))
+      });
+      setBlogs(blogs.concat(blog));
       setErrorMessage({ text: `a new blog ${blog.title}`, type: "success" });
-      blogRef.current.toggleVisibility()
+      blogRef.current.toggleVisibility();
     } catch (exception) {
       setErrorMessage({ text: `wrong data`, type: "error" });
-      console.log('exception', exception)
+      console.log("exception", exception);
     }
-  }
-
+  };
 
   return (
     <div>
@@ -73,14 +72,16 @@ const App = () => {
       ) : (
         <>
           <p>{user.name} logged in</p>
-          
+
           <button onClick={handleLogout}>logout</button>
           <Togglable buttonLabel="new blog" ref={blogRef}>
             <BlogForm handleBlogForm={handleBlogForm} />
           </Togglable>
-          {blogs.map((blog) => (
-            <Blog key={blog.id} blog={blog} />
-          ))}
+          <ul>
+            {blogs.map((blog) => (
+              <Blog key={blog.id} blog={blog} />
+            ))}
+          </ul>
         </>
       )}
     </div>
--- part5/bloglist-f2E/src/components/Blog.js
+++ part5/bloglist-f2E/src/components/Blog.js
@@ -1,8 +1,8 @@
 import React from 'react'
 const Blog = ({ blog }) => (
-  <div>
+  <li>
     {blog.title}
-  </div>
+  </li>
 )
 
 export default Blog
