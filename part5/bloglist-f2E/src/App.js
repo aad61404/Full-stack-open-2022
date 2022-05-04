d1c095dd1ecc44588cbe3384b5ebd8a0ca6c7cc8
Jayson Cheng <jason.cheng@onelab.tw>
2022-05-04 14:17:06 +0800

    Exercise 5.4 create blog Notification

--- part5/bloglist-f2E/src/App.js
+++ part5/bloglist-f2E/src/App.js
@@ -2,14 +2,15 @@ import React, { useState, useEffect } from "react";
 import Blog from "./components/Blog";
 import LoginForm from "./components/LoginForm";
 import BlogForm from './components/BlogForm'
-// import Notification from "./components/Notification";
+import Notification from "./components/Notification";
 import blogService from "./services/blogs";
 import loginService from "./services/login";
+import "./app.css"
 
 const App = () => {
   const [blogs, setBlogs] = useState([]);
   const [user, setUser] = useState(null);
-  // const [errorMessage, setErrorMessage] = useEffect(null)
+  const [errorMessage, setErrorMessage] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then((blogs) => setBlogs(blogs));
@@ -31,14 +32,9 @@ const App = () => {
       blogService.setToken(user.token)
       setUser(user)
       window.localStorage.setItem('user', JSON.stringify(user))
+      setErrorMessage({ text: `Weclome ${user.name}`, type: "success" });
     } catch (exception) {
-      // console.log('exception:', exception)
-      /*
-      setErrorMessage('Wrong credentials')
-      setTimeout(() => {
-        setErrorMessage(null)
-      }, 5000)
-      */
+      setErrorMessage({ text: `wrong username or password`, type: "error" });
     }
   }
 
@@ -57,7 +53,9 @@ const App = () => {
         url,
       })
       setBlogs(blogs.concat(blog))
+      setErrorMessage({ text: `a new blog ${blog.title}`, type: "success" });
     } catch (exception) {
+      setErrorMessage({ text: `wrong data`, type: "error" });
       console.log('exception', exception)
     }
   }
@@ -66,12 +64,13 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
+      <Notification message={errorMessage} />
       {user === null ? (
         <LoginForm handleLogin={handleLogin} />
       ) : (
         <>
           <p>{user.name} logged in</p>
-          {/* <Notification message={errorMessage} /> */}
+          
           <button onClick={handleLogout}>logout</button>
           <BlogForm handleBlogForm={handleBlogForm} />
           {blogs.map((blog) => (
