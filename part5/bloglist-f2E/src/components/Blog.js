e8e58280aeeb7508d174417629608473eea3aa80
Jayson Cheng <jason.cheng@onelab.tw>
2022-05-04 17:16:11 +0800

    Exercise 5.7 toggle visible blog

--- part5/bloglist-f2E/src/components/Blog.js
+++ part5/bloglist-f2E/src/components/Blog.js
@@ -1,8 +1,36 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <li>
-    {blog.title}
-  </li>
-)
+import React, { useState } from 'react'
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} <button onClick={toggleVisibility}>view</button>
+      </div>
+      {visible && (
+        <div>
+          <p>{blog.url}</p>
+          <p>
+            likes {blog.likes} <button>like</button>
+          </p>
+          <p>{blog.author}</p>
+        </div>
+      )}
+    </div>
+  )
+}
 
 export default Blog
