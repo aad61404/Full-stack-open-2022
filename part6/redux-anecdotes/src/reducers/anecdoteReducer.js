7c5285a2d16991e5d4276bec4b7a1d93612bd122
Jayson Cheng <jason.cheng@onelab.tw>
2022-05-06 17:26:40 +0800

    Exercise 6.15 redux-thunk async action

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,3 +1,5 @@
+import anecdoteService from '../services/anecdotes'
+
 const reducer = (state = [], action) => {
   switch (action.type) {
     case "NEW_VOTE":
@@ -36,11 +38,14 @@ export const addAnecdote = (anecdote) => {
   };
 };
 
-export const initializeAnecdotes = (anecdotes) => {
-  return {
-    type: "INIT_ANECDOTES",
-    data: anecdotes,
-  };
-};
+export const initializeAnecdotes = () => {
+  return async (dispatch) => {
+    const anecdotes = await anecdoteService.getAll()
+    dispatch({
+      type: 'INIT_ANECDOTES',
+      data: anecdotes,
+    })
+  }
+}
 
 export default reducer;
