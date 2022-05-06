6153ad3c948209da91fddc0fd242acbc56cd8c77
Jayson Cheng <jason.cheng@onelab.tw>
2022-05-06 15:06:02 +0800

    Exercise 6.6~6.7 seperate submitAnecdote to AnecdoteForm

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { addVoteOf, addAnecdote } from "./reducers/anecdoteReducer";
+import AnecdoteForm from "./components/AnecdoteForm";
 
 const App = () => {
   const anecdotes = useSelector((state) => state);
@@ -8,14 +9,6 @@ const App = () => {
 
   const vote = (id) => {
     dispatch(addVoteOf(id));
-  };
-
-  const submitAnecdote = (event) => {
-    event.preventDefault();
-    const anecdote = event.target.anecdote.value;
-    console.log("anecdote:", anecdote);
-    event.target.anecdote.value = "";
-    dispatch(addAnecdote(anecdote));
   };
 
   return (
@@ -32,13 +25,7 @@ const App = () => {
             </div>
           </div>
         ))}
-      <h2>create new</h2>
-      <form onSubmit={submitAnecdote}>
-        <div>
-          <input name="anecdote" />
-        </div>
-        <button>create</button>
-      </form>
+      <AnecdoteForm />
     </div>
   );
 };
